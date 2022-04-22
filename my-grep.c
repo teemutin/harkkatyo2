14c46d5668ba4aa1bfff02f3b8ea9a9e35095886
Teemutin <teemu.eloranta@student.lut.fi>
2022-04-22 03:01:06 -0700

    kaikki vaatimukset toimii

--- my-grep.c
+++ my-grep.c
@@ -24,26 +24,29 @@ if (argc == 2) {
     exit(0);
 
 }
-//Loop through all the given arguments, as input files
-while(argv[i] != NULL) {
-    //Open and test the file opened correctly
-    file = fopen(argv[i], "r");
-    if (file == NULL){
-        fprintf(stderr, "my-grep: cannot open file\n");
-        exit(1);
+//If 1 or more files provided
+if (argc > 2) {
+    //Loop through all the given arguments, as input files
+    while(argv[i] != NULL) {
+        //Open and test the file opened correctly
+        file = fopen(argv[i], "r");
+        if (file == NULL){
+            fprintf(stderr, "my-grep: cannot open file\n");
+            exit(1);
+        }
+        //set the pattern
+        strcpy(pattern,argv[1]);
+        //loop through the file, if pattern match is made, print the line
+        while(!feof(file)) {
+            fgets(string1,2000,file);
+            if(strstr(string1,pattern))
+                printf("%s",string1);
+        }
+        
+        //close file and get next argument in loop
+        fclose(file);
+        i++;
     }
-    //set the pattern
-    strcpy(pattern,argv[1]);
-    //loop through the file, if pattern match is made, print the line
-    while(!feof(file)) {
-        fgets(string1,2000,file);
-        if(strstr(string1,pattern))
-            printf("%s",string1);
-    }
-    
-    //close file and get next argument in loop
-    fclose(file);
-    i++;
 }
 exit(0);
 
